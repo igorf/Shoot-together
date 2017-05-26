b81517cefd25de9fe2853f17f0a4e43d684e5e4a
Dmitry Karyaev <lesser.daemon@gmail.com>
2017-05-26 16:58:01 +0400

    gradle project

---
+++ src/main/java/com/github/igorf/shoot/ShootApplication.java
@@ -0,0 +1,12 @@
+package com.github.igorf.shoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShootApplication.class, args);
+	}
+}
